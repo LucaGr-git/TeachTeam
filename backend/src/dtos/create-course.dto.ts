5ccc303e73a0a92b7aef1d24cc8fe232ed5b4fb6
AKhan-SE <s4095471@student.rmit.edu.au>
2025-06-07 21:32:28 +1000

    Create course dto

---
+++ backend/src/dtos/create-course.dto.ts
@@ -0,0 +1,22 @@
+import { IsBoolean, IsNotEmpty } from "class-validator";
+import { Length } from "class-validator";
+import { IsString } from "class-validator";
+
+export class CreateCourseDTO {
+    @IsString()
+    @Length(8, 10)
+    @IsNotEmpty()
+    courseCode: string;
+
+    @IsString()
+    @IsNotEmpty()
+    courseTitle: string;
+
+    @IsBoolean()
+    @IsNotEmpty()
+    partTimeFriendly: boolean;
+
+    @IsBoolean()
+    @IsNotEmpty()
+    fullTimeFriendly: boolean;
+}
