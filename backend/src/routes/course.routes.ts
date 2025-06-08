42057b8c6fb12a52c4eee165bda73765c0ad3a26
Luca grosso <S4093817@student.rmit.edu.au>
2025-06-08 19:09:25 +1000

    fixed course routes

---
+++ backend/src/routes/course.routes.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { CourseController } from "../controller/CourseController";
+
+const router = Router();
+const controller = new CourseController();
+
+router.get('/courses', (req, res) => controller.getAll(req, res));
+
+router.get("/courses/:courseCode", (req, res) => controller.getCourseByCode(req, res));
+
+router.post("/courses", (req, res) => controller.createCourse(req, res));
+
+router.put("/courses/:courseCode", (req, res) => controller.updateCourse(req, res));
+
+router.delete("'/courses/:courseCode'", (req, res) => controller.deleteCourse(req, res));
+
+
+router.get("/courses/:courseCode/lecturers", (req, res) => controller.getLecturerByCourseCode(req, res));
+
+router.get("/lecturers/:lecturerEmail/courses", (req, res) => controller.getCourseCodeByLecturer(req, res)); 
+
+router.post("/courses/:courseCode/lecturers", (req, res) => controller.createCourseLecturer(req, res));
+
+
+router.get("/courses/:courseCode/applications", (req, res) => controller.getTutorApplicationByCourseCode(req, res));
+
+router.get("/applications/:tutorEmail/courses", (req, res) => controller.getCourseCodeByTutorApplication(req, res)); 
+
+router.post("/courses/:courseCode/applications", (req, res) => controller.createTutorApplication(req, res));
+
+router.delete("/applications/:tutorEmail/:courseCode", (req, res) => controller.deleteTutorApplication(req, res));
+
+
+router.get("/courses/:courseCode/shortlistedTutors", (req, res) => controller.getShortlistedTutorByCourseCode(req, res));
+
+router.get("/shortlisted-tutors/:tutorEmail/courses", (req, res) => controller.getCourseCodeByShortlistedTutorEmail(req, res));
+
+router.post("/courses/:courseCode/shortlistedTutors", (req, res) => controller.createShortlistedTutor(req, res));
+
+router.delete("/courses/:courseCode/shortlistedTutors/:tutorEmail", (req, res) => controller.deleteShortlistedTutor(req, res));
+
+
+// todo change course code functions to reference entity not jsut code where applicable
+// todo also pluralize e.g.  getShortlistedTutorByCourseCode --> getShortlistedTutorsByCourseCode
+// todo add all functions to routes
+export default router;
