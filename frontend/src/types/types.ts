e623b05bea297255e338d286f2b27e628027320a
Luca grosso <S4093817@student.rmit.edu.au>
2025-06-10 19:31:58 +1000

    changed types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -69,7 +69,7 @@ export interface TutorApplication {
 
 export interface CourseLecturer {
     courseCode: string;
-    tuthorEmail: string;
+    lectuerEmail: string;
 }
 
 export interface CourseTutor{
